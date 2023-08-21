f93fe7e7640d7cd7bfcf1e360ace1f8f2f772a89
MUJIMERGE <smaryn101@gmail.com>
2023-08-21 23:09:17 +0900

    Users App 2

--- houses/models.py
+++ houses/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.conf import settings
 
 class House(models.Model):
     
     """Model Definition for Houses"""
     
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField(verbose_name="Price")
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price",
+        help_text="Positive Numbers Only"
+    )
     description = models.TextField()
     address = models.CharField(max_length=140)
     pets_allowed = models.BooleanField(
@@ -13,6 +17,8 @@ class House(models.Model):
         default=True, 
         help_text="Does this house allow pets?"
     )
+    # owner = models.ForeignKey("users.User", on_delete=models.SET_NULL)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False)
+    last_name = models.CharField(max_length=150, editable=False)
+    name = models.CharField(max_length=150, default="") # Custom
+    is_host = models.BooleanField(default=False)        # Custom
