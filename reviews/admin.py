0afd5dfec3381d9d72ab04bf74889cdbdbad71a3
MUJIMERGE <smaryn101@gmail.com>
2023-09-02 21:14:52 +0900

    Custom Filters

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,6 +1,48 @@
 from django.contrib import admin
 from .models import Review
 
+class WordFilter(admin.SimpleListFilter):
+    title = "Filter by words!"
+    
+    parameter_name = "word"
+    
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("great", "Great"),
+            ("awesome", "Awesome")
+        ]
+        
+    def queryset(self, request, reviews):
+        word = self.value()
+        if word:
+            return reviews.filter(payload__contains=word)
+        else:
+            reviews
+            
+            
+class RatingFilter(admin.SimpleListFilter):
+    title = "Filter by Good or Bad rating!"
+    
+    parameter_name = "gbrating"
+    
+    def lookups(self, request, model_admin):
+        return [
+            ("good", "Good"),
+            ("bad", "Bad")
+        ]
+        
+    def queryset(self, request, reviews):
+        gbrating = self.value()
+        if gbrating:
+            if gbrating == "good":
+                return reviews.filter(rating__gte=3)
+            else:
+                return reviews.filter(rating__lt=3)
+        else:
+            reviews
+    
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     
@@ -10,5 +52,10 @@ class ReviewAdmin(admin.ModelAdmin):
     )
     
     list_filter = (
+        WordFilter,
+        RatingFilter,
         "rating",
+        "user__is_host",
+        "room__category",
+        "room__pet_friendly",
     )
