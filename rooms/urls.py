181c949a940d0ccd6eeb70ec93a201c87f90476b
MUJIMERGE <smaryn101@gmail.com>
2023-09-05 21:00:58 +0900

    DoesNotExist(will be erase)

--- rooms/urls.py
+++ rooms/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.see_all_rooms),
-    path("<int:room_id>", views.see_one_room),
+    path("<int:room_pk>", views.see_one_room),
     # path("<int:room_id>/<str:room_name>", views.see_one_room),
     # path("<str:room_name>", views.s),
 ]
