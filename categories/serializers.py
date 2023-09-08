5736fb1acc25d6269ed4601b8d72019d0bb8998b
MUJIMERGE <smaryn101@gmail.com>
2023-09-08 23:35:45 +0900

    serializer save()

--- categories/serializers.py
+++ categories/serializers.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
+from .models import Category
 
 class CategorySerializer(serializers.Serializer):
     
-    pk = serializers.IntegerField(read_only=True)
+    pk = serializers.IntegerField(
+        read_only=True
+    )
+    
     name = serializers.CharField(
         required=True,
         max_length=50,
     )
-    kind = serializers.CharField(max_length=15)
-    created_at = serializers.DateTimeField(read_only=True)
+    
+    kind = serializers.ChoiceField(
+        choices=Category.CategoryKindChoices.choices,
+    )
+    
+    created_at = serializers.DateTimeField(
+        read_only=True
+    )
+    
+    def create(self, validated_data):
+        return Category.objects.create(**validated_data)
+
+
+# {
+#     "name": "Category from DRF",
+#     "kind": "ffff"
+# }
