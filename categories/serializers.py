4b75c0ee21a9722cf36136dbbc9572ed0c329098
MUJIMERGE <smaryn101@gmail.com>
2023-09-09 23:55:13 +0900

    update, delete

--- categories/serializers.py
+++ categories/serializers.py
@@ -22,9 +22,15 @@ class CategorySerializer(serializers.Serializer):
     
     def create(self, validated_data):
         return Category.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.kind = validated_data.get("kind", instance.kind)
+        instance.save()
+        return instance
 
 
 # {
 #     "name": "Category from DRF",
-#     "kind": "ffff"
+#     "kind": "rooms"
 # }
